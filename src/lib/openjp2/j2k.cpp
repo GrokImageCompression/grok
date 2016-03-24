0da5fb4c28c7b0fc5581fdc339776fbf07e3ea58
Aaron Boxer <boxerab@gmail.com>
2016-03-24 13:36:07 -0400

    restore a single tile memory optimization

--- src/lib/openjp2/j2k.cpp
+++ src/lib/openjp2/j2k.cpp
@@ -9214,12 +9214,8 @@ static bool opj_j2k_allocate_tile_element_cstr_index(opj_j2k_t *p_j2k)
 
 static bool opj_j2k_needs_copy_tile_data(opj_j2k_t *p_j2k, uint32_t num_tiles)
 {
-	return true;
-
     /* single tile, RGB images only*/
-    bool copy_tile_data = (num_tiles> 1) ||
-                          p_j2k->m_output_image->numcomps != 3 ||
-                          p_j2k->m_tcd->tile->numcomps != 3;
+    bool copy_tile_data = (num_tiles> 1) ;
 
     uint32_t i = 0;
 
