03827bff031d0abf032298607405ebbba46e84ab
Aaron Boxer <boxerab@gmail.com>
2016-04-20 15:08:58 -0400

    avoid another overflow situation

--- src/lib/openjp2/pi.cpp
+++ src/lib/openjp2/pi.cpp
@@ -651,18 +651,19 @@ static void opj_get_encoding_parameters(	const opj_image_t *p_image,
 
         /* use custom size for precincts */
         for (resno = 0; resno < l_tccp->numresolutions; ++resno) {
-            uint32_t l_dx, l_dy;
 
             /* precinct width and height */
             l_pdx = l_tccp->prcw[resno];
             l_pdy = l_tccp->prch[resno];
 
-            l_dx = l_img_comp->dx * (1u << (l_pdx + l_tccp->numresolutions - 1 - resno));
-            l_dy = l_img_comp->dy * (1u << (l_pdy + l_tccp->numresolutions - 1 - resno));
+            uint64_t l_dx = l_img_comp->dx * ((uint64_t)1u << (l_pdx + l_tccp->numresolutions - 1 - resno));
+			uint64_t l_dy = l_img_comp->dy * ((uint64_t)1u << (l_pdy + l_tccp->numresolutions - 1 - resno));
 
             /* take the minimum size for dx for each comp and resolution */
-            *p_dx_min = opj_uint_min(*p_dx_min, l_dx);
-            *p_dy_min = opj_uint_min(*p_dy_min, l_dy);
+			if (l_dx < UINT_MAX)
+				*p_dx_min = opj_uint_min(*p_dx_min, (uint32_t)l_dx);
+			if (l_dy < UINT_MAX)
+				*p_dy_min = opj_uint_min(*p_dy_min, (uint32_t)l_dy);
 
             /* various calculations of extents */
             l_level_no = l_tccp->numresolutions - 1 - resno;
