6b2772cc2b79deff4450352593fe01bac0290f25
Aaron Boxer <boxerab@gmail.com>
2016-04-26 16:47:29 -0400

    tweaked thread pool

--- src/lib/openjp2/ThreadPool.h
+++ src/lib/openjp2/ThreadPool.h
@@ -57,23 +57,20 @@ inline ThreadPool::ThreadPool(size_t threads)
     :   stop(false)
 {
     for(size_t i = 0;i<threads;++i)
-        workers.emplace_back(
-            [this]
+        workers.emplace_back([this]
             {
-                for(;;)
+                while(true)
                 {
                     std::function<void()> task;
-
                     {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        std::unique_lock<std::mutex> lock(queue_mutex);
                         this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty())
+							[this]{ return stop || !tasks.empty(); 	});
+                        if(stop)
                             return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
+                        task = std::move(tasks.front());
+                        tasks.pop();
                     }
-
                     task();
                 }
             }
