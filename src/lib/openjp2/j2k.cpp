ebbf7f02e28a4deee44d2f0944f8e6e7e6981002
Aaron Boxer <boxerab@gmail.com>
2016-06-19 17:08:49 -0400

    improved debug info when remaining bytes in stream is less than SOT length

--- src/lib/openjp2/j2k.cpp
+++ src/lib/openjp2/j2k.cpp
@@ -4156,7 +4156,11 @@ static bool opj_j2k_read_sod (opj_j2k_t *p_j2k,
         /* If we are here, we'll try to read the data after allocation */
         /* Check enough bytes left in stream before allocation */
         if ((int64_t)p_j2k->m_specific_param.m_decoder.m_sot_length > opj_stream_get_number_byte_left(p_stream)) {
-            opj_event_msg(p_manager, EVT_ERROR, "Tile part length size inconsistent with stream length\n");
+            opj_event_msg(p_manager, 
+						EVT_ERROR, 
+						"Tile part length size %d inconsistent with stream length %lld\n", 
+						p_j2k->m_specific_param.m_decoder.m_sot_length, 
+						opj_stream_get_number_byte_left(p_stream));
             return false;
         }
     } else {
