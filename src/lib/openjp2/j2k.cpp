1789804fd22ba5f33e1cc504773813df2e423e6d
Aaron Boxer <boxerab@gmail.com>
2016-08-08 13:09:56 -0400

    re-enable reading of PLM marker

--- src/lib/openjp2/j2k.cpp
+++ src/lib/openjp2/j2k.cpp
@@ -3330,56 +3330,43 @@ static bool opj_j2k_read_plm (  opj_j2k_t *p_j2k,
     assert(p_header_data != 00);
     assert(p_j2k != 00);
     assert(p_manager != 00);
+	uint32_t l_Zplm, l_Nplm, l_tmp, l_packet_len = 0, i;
 
     if (p_header_size < 1) {
         opj_event_msg(p_manager, EVT_ERROR, "Error reading PLM marker\n");
         return false;
     }
-    /* Do not care of this at the moment since only local variables are set here */
-    /*
+
     opj_read_bytes(p_header_data,&l_Zplm,1);                                        // Zplm
     ++p_header_data;
     --p_header_size;
 
-    while
-            (p_header_size > 0)
-    {
-            opj_read_bytes(p_header_data,&l_Nplm,1);                                // Nplm
+    while  (p_header_size > 0)   {
+        opj_read_bytes(p_header_data,&l_Nplm,1);                                // Nplm
+        ++p_header_data;
+        p_header_size -= (1+l_Nplm);
+        if (p_header_size < 0)     {
+             opj_event_msg(p_manager, EVT_ERROR, "Error reading PLM marker\n");
+             return false;
+        }
+        for (i = 0; i < l_Nplm; ++i)       {
+            opj_read_bytes(p_header_data,&l_tmp,1);                         // Iplm_ij
             ++p_header_data;
-            p_header_size -= (1+l_Nplm);
-            if
-                    (p_header_size < 0)
-            {
-                    opj_event_msg(p_manager, EVT_ERROR, "Error reading PLM marker\n");
-                    return false;
+            // take only the last seven bytes
+            l_packet_len |= (l_tmp & 0x7f);
+            if  (l_tmp & 0x80)        {
+                l_packet_len <<= 7;
+            }  else   {
+					// store packet length and proceed to next packet
+                    l_packet_len = 0;
             }
-            for
-                    (i = 0; i < l_Nplm; ++i)
-            {
-                    opj_read_bytes(p_header_data,&l_tmp,1);                         // Iplm_ij
-                    ++p_header_data;
-                    // take only the last seven bytes
-                    l_packet_len |= (l_tmp & 0x7f);
-                    if
-                            (l_tmp & 0x80)
-                    {
-                            l_packet_len <<= 7;
-                    }
-                    else
-                    {
-            // store packet length and proceed to next packet
-                            l_packet_len = 0;
-                    }
-            }
-            if
-                    (l_packet_len != 0)
-            {
-                    opj_event_msg(p_manager, EVT_ERROR, "Error reading PLM marker\n");
-                    return false;
-            }
+        }
+        if (l_packet_len != 0)      {
+            opj_event_msg(p_manager, EVT_ERROR, "Error reading PLM marker\n");
+            return false;
+        }
     }
-    */
-    return true;
+     return true;
 }
 
 /**
