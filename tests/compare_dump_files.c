d0fd29feb8850bf630935255405ba8486ed1e6f2
Aaron Boxer <boxerab@gmail.com>
2016-09-07 20:20:10 -0400

    sanity check on tile coords

--- tests/compare_dump_files.c
+++ tests/compare_dump_files.c
@@ -115,10 +115,10 @@ int main(int argc, char **argv)
     test_cmp_parameters inParam;
     FILE *fbase=NULL, *ftest=NULL;
     int same = 0;
-    char lbase[256];
-    char strbase[256];
-    char ltest[256];
-    char strtest[256];
+  char lbase[512];
+  char strbase[512];
+  char ltest[512];
+  char strtest[512];
 
     if( parse_cmdline_cmp(argc, argv, &inParam) == 1 ) {
         compare_dump_files_help_display();
@@ -147,9 +147,9 @@ int main(int argc, char **argv)
     printf("Ok.\n");
 
     while (fgets(lbase, sizeof(lbase), fbase) && fgets(ltest,sizeof(ltest),ftest)) {
-        int nbase = sscanf(lbase, "%255[^\r\n]", strbase);
-        int ntest = sscanf(ltest, "%255[^\r\n]", strtest);
-        assert( nbase != 255 && ntest != 255 );
+    int nbase = sscanf(lbase, "%511[^\r\n]", strbase);
+    int ntest = sscanf(ltest, "%511[^\r\n]", strtest);
+    assert( nbase != 511 && ntest != 511 );
         if( nbase != 1 || ntest != 1 ) {
             fprintf(stderr, "could not parse line from files\n" );
             goto cleanup;
