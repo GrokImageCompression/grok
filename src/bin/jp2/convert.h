394b92e9d6f5739f0bc27190735102f93c225ba5
Aaron Boxer <boxerab@gmail.com>
2017-03-02 09:35:19 -0500

    imagetotif : do not try to infer alpha channels from number of components

--- src/bin/jp2/convert.h
+++ src/bin/jp2/convert.h
@@ -106,7 +106,7 @@ extern "C" {
 
 	/* TIFF conversion*/
 	opj_image_t* tiftoimage(const char *filename, opj_cparameters_t *parameters, bool applyICC);
-	int imagetotif(opj_image_t *image, const char *outfile, uint32_t compression, bool enableInferredAlpha);
+	int imagetotif(opj_image_t *image, const char *outfile, uint32_t compression);
 	/**
 	Load a single image component encoded in PGX file format
 	@param filename Name of the PGX file to load
