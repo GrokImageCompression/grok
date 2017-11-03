4cb816d24e193050dd9d8c79079ee7b6265bd066
Aaron Boxer <boxerab@gmail.com>
2017-11-02 20:27:47 -0400

    add null pointer check on image when calling decode callback

--- src/lib/openjp2/grok.cpp
+++ src/lib/openjp2/grok.cpp
@@ -1284,6 +1284,9 @@ int32_t grok_plugin_internal_decode_callback(PluginDecodeCallbackInfo* info){
 			info->image = grokInfo.image;
 			info->l_stream = grokInfo.l_stream;
 			info->l_codec = grokInfo.l_codec;
+			// something went wrong....
+			if (!info->image)
+				return -1;
 		}
     } 
 	// post T1 calls are routed to post-decode callback
