7e57baad2f496cff0fa1e151f7b6a66e1ebc477b
Aaron Boxer <boxerab@gmail.com>
2018-01-15 16:09:40 -0500

    update copyright

--- src/lib/openjp2/vector.cpp
+++ src/lib/openjp2/vector.cpp
@@ -1,5 +1,5 @@
 /**
-*    Copyright (C) 2016-2017 Grok Image Compression Inc.
+*    Copyright (C) 2016-2018 Grok Image Compression Inc.
 *
 *    This source code is free software: you can redistribute it and/or  modify
 *    it under the terms of the GNU Affero General Public License, version 3,
