1125ec708a1a96b706f9586b62fb46d0d7a58a54
Aaron Boxer <boxerab@gmail.com>
2018-02-14 22:49:07 -0500

    imagetopgx: add check that file name is at least five characters long

--- src/bin/jp2/PGXFormat.cpp
+++ src/bin/jp2/PGXFormat.cpp
@@ -307,7 +307,11 @@ static int imagetopgx(opj_image_t * image, const char *outfile)
 		size_t res;
 		const size_t olen = strlen(outfile);
 		if (olen > 4096) {
-			fprintf(stderr, "[ERROR] imagetopgx: output file name larger than 4096.");
+			fprintf(stderr, "[ERROR] imagetopgx: output file name size larger than 4096.");
+			goto beach;
+		}
+		if (olen < 5) {
+			fprintf(stderr, "[ERROR] imagetopgx: output file name size less than 5.");
 			goto beach;
 		}
 		const size_t dotpos = olen - 4;
