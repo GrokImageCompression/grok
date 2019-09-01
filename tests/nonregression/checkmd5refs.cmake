aeb534556f5f84bbf6865d1c334707fe99f5958e
Aaron Boxer <boxerab@gmail.com>
2019-08-31 22:10:15 -0400

    update license

--- tests/nonregression/checkmd5refs.cmake
+++ tests/nonregression/checkmd5refs.cmake
@@ -1,4 +1,4 @@
-#    Copyright (C) 2016-2017 Grok Image Compression Inc.
+#    Copyright (C) 2016-2019 Grok Image Compression Inc.
 #
 #    This source code is free software: you can redistribute it and/or  modify
 #    it under the terms of the GNU Affero General Public License, version 3,
