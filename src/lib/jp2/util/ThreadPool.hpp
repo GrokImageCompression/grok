c00c4be103e8af7cb295d9a9a4f01a1bb4e3a838
Aaron Boxer <boxerab@gmail.com>
2020-03-12 18:38:36 -0400

    license: add copyright

--- src/lib/jp2/util/ThreadPool.hpp
+++ src/lib/jp2/util/ThreadPool.hpp
@@ -1,5 +1,22 @@
-#ifndef THREAD_POOL_H
-#define THREAD_POOL_H
+/*
+ *    Copyright (C) 2016-2020 Grok Image Compression Inc.
+ *
+ *    This source code is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This source code is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#pragma once
 
 #include <vector>
 #include <queue>
@@ -111,5 +128,3 @@ inline ThreadPool::~ThreadPool()
     for(std::thread &worker: workers)
         worker.join();
 }
-
-#endif
