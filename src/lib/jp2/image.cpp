74c26ba5579233a97cc893b5871cf6f486a034fd
Aaron Boxer <boxerab@gmail.com>
2020-03-25 08:11:15 -0400

    cleanup

--- src/lib/jp2/image.cpp
+++ src/lib/jp2/image.cpp
@@ -52,6 +52,7 @@
 grk_image *  GRK_CALLCONV grk_image_create(uint32_t numcmpts,
 		 grk_image_cmptparm  *cmptparms, GRK_COLOR_SPACE clrspc) {
 	auto image = (grk_image * ) grk::grk_calloc(1, sizeof(grk_image));
+
 	if (image) {
 		image->color_space = clrspc;
 		image->numcomps = numcmpts;
@@ -65,7 +66,8 @@ grk_image *  GRK_CALLCONV grk_image_create(uint32_t numcmpts,
 		}
 		/* create the individual image components */
 		for (uint32_t compno = 0; compno < numcmpts; compno++) {
-			 grk_image_comp  *comp = &image->comps[compno];
+			auto comp = &image->comps[compno];
+
 			comp->dx = cmptparms[compno].dx;
 			comp->dy = cmptparms[compno].dy;
 			comp->w = cmptparms[compno].w;
@@ -116,39 +118,39 @@ grk_image *  grk_image_create0(void) {
 /**
  * Updates the components characteristics of the image from the coding parameters.
  *
- * @param p_image_header	the image header to update.
+ * @param image_header	the image header to update.
  * @param p_cp				the coding parameters from which to update the image.
  */
-void grk_image_comp_header_update(grk_image *p_image_header,
+void grk_image_comp_header_update(grk_image *image_header,
 		const grk_coding_parameters *p_cp) {
 
 	//1. calculate canvas coordinates of image
-	uint32_t l_x0 = std::max<uint32_t>(p_cp->tx0, p_image_header->x0);
-	uint32_t l_y0 = std::max<uint32_t>(p_cp->ty0, p_image_header->y0);
+	uint32_t x0 = std::max<uint32_t>(p_cp->tx0, image_header->x0);
+	uint32_t y0 = std::max<uint32_t>(p_cp->ty0, image_header->y0);
 
 	/* validity of p_cp members used here checked in j2k_read_siz. Can't overflow. */
-	uint32_t l_x1 = p_cp->tx0 + (p_cp->tw - 1U) * p_cp->tdx;
-	uint32_t l_y1 = p_cp->ty0 + (p_cp->th - 1U) * p_cp->tdy;
+	uint32_t x1 = p_cp->tx0 + (p_cp->tw - 1U) * p_cp->tdx;
+	uint32_t y1 = p_cp->ty0 + (p_cp->th - 1U) * p_cp->tdy;
 
 	 /* use add saturated to prevent overflow */
-	l_x1 = std::min<uint32_t>(uint_adds(l_x1, p_cp->tdx), p_image_header->x1);
-	l_y1 = std::min<uint32_t>(uint_adds(l_y1, p_cp->tdy), p_image_header->y1);
+	x1 = std::min<uint32_t>(uint_adds(x1, p_cp->tdx), image_header->x1);
+	y1 = std::min<uint32_t>(uint_adds(y1, p_cp->tdy), image_header->y1);
 
 	// 2. convert from canvas to tile coordinates, taking into account
 	// resolution reduction
 	uint32_t reduce = p_cp->m_coding_param.m_dec.m_reduce;
-	for (uint32_t i = 0; i < p_image_header->numcomps; ++i) {
-		auto l_img_comp = p_image_header->comps + i;
-		uint32_t l_comp_x0 = ceildiv<uint32_t>(l_x0, l_img_comp->dx);
-		uint32_t l_comp_y0 = ceildiv<uint32_t>(l_y0, l_img_comp->dy);
-		uint32_t l_comp_x1 = ceildiv<uint32_t>(l_x1, l_img_comp->dx);
-		uint32_t l_comp_y1 = ceildiv<uint32_t>(l_y1, l_img_comp->dy);
-		uint32_t l_width = uint_ceildivpow2(l_comp_x1 - l_comp_x0,reduce);
-		uint32_t l_height = uint_ceildivpow2(l_comp_y1 - l_comp_y0,reduce);
-		l_img_comp->w = l_width;
-		l_img_comp->h = l_height;
-		l_img_comp->x0 = l_comp_x0;
-		l_img_comp->y0 = l_comp_y0;
+	for (uint32_t i = 0; i < image_header->numcomps; ++i) {
+		auto img_comp = image_header->comps + i;
+		uint32_t comp_x0 = ceildiv<uint32_t>(x0, img_comp->dx);
+		uint32_t comp_y0 = ceildiv<uint32_t>(y0, img_comp->dy);
+		uint32_t comp_x1 = ceildiv<uint32_t>(x1, img_comp->dx);
+		uint32_t comp_y1 = ceildiv<uint32_t>(y1, img_comp->dy);
+		uint32_t width = uint_ceildivpow2(comp_x1 - comp_x0,reduce);
+		uint32_t height = uint_ceildivpow2(comp_y1 - comp_y0,reduce);
+		img_comp->w = width;
+		img_comp->h = height;
+		img_comp->x0 = comp_x0;
+		img_comp->y0 = comp_y0;
 	}
 }
 
@@ -156,92 +158,88 @@ void grk_image_comp_header_update(grk_image *p_image_header,
  * Copy only header of image and its component header (no data are copied)
  * if dest image have data, they will be freed
  *
- * @param	p_image_src		the src image
- * @param	p_image_dest	the dest image
+ * @param	image_src		the src image
+ * @param	image_dest	the dest image
  *
  */
-void grk_copy_image_header(const grk_image *p_image_src,
-		grk_image *p_image_dest) {
-	assert(p_image_src != nullptr);
-	assert(p_image_dest != nullptr);
+void grk_copy_image_header(const grk_image *image_src,grk_image *image_dest) {
+	assert(image_src != nullptr);
+	assert(image_dest != nullptr);
 
-	p_image_dest->x0 = p_image_src->x0;
-	p_image_dest->y0 = p_image_src->y0;
-	p_image_dest->x1 = p_image_src->x1;
-	p_image_dest->y1 = p_image_src->y1;
+	image_dest->x0 = image_src->x0;
+	image_dest->y0 = image_src->y0;
+	image_dest->x1 = image_src->x1;
+	image_dest->y1 = image_src->y1;
 
-	if (p_image_dest->comps) {
-		grk_image_all_components_data_free(p_image_dest);
-		grok_free(p_image_dest->comps);
-		p_image_dest->comps = nullptr;
+	if (image_dest->comps) {
+		grk_image_all_components_data_free(image_dest);
+		grok_free(image_dest->comps);
+		image_dest->comps = nullptr;
 	}
-	p_image_dest->numcomps = p_image_src->numcomps;
-	p_image_dest->comps = ( grk_image_comp  * ) grk_malloc(
-			p_image_dest->numcomps * sizeof( grk_image_comp) );
-	if (!p_image_dest->comps) {
-		p_image_dest->comps = nullptr;
-		p_image_dest->numcomps = 0;
+	image_dest->numcomps = image_src->numcomps;
+	image_dest->comps = ( grk_image_comp  * ) grk_malloc(
+			image_dest->numcomps * sizeof( grk_image_comp) );
+	if (!image_dest->comps) {
+		image_dest->comps = nullptr;
+		image_dest->numcomps = 0;
 		return;
 	}
 
-	for (uint32_t compno = 0; compno < p_image_dest->numcomps; compno++) {
-		memcpy(&(p_image_dest->comps[compno]), &(p_image_src->comps[compno]),
+	for (uint32_t compno = 0; compno < image_dest->numcomps; compno++) {
+		memcpy(&(image_dest->comps[compno]), &(image_src->comps[compno]),
 				sizeof( grk_image_comp) );
-		p_image_dest->comps[compno].data = nullptr;
+		image_dest->comps[compno].data = nullptr;
 	}
 
-	p_image_dest->color_space = p_image_src->color_space;
-	p_image_dest->icc_profile_len = p_image_src->icc_profile_len;
-	if (p_image_dest->icc_profile_len) {
-		p_image_dest->icc_profile_buf = grk_buffer_new(
-				p_image_dest->icc_profile_len);
-		memcpy(p_image_dest->icc_profile_buf, p_image_src->icc_profile_buf,
-				p_image_src->icc_profile_len);
+	image_dest->color_space = image_src->color_space;
+	image_dest->icc_profile_len = image_src->icc_profile_len;
+	if (image_dest->icc_profile_len) {
+		image_dest->icc_profile_buf = grk_buffer_new(
+				image_dest->icc_profile_len);
+		memcpy(image_dest->icc_profile_buf, image_src->icc_profile_buf,
+				image_src->icc_profile_len);
 	} else
-		p_image_dest->icc_profile_buf = nullptr;
+		image_dest->icc_profile_buf = nullptr;
 
 	return;
 }
 
-bool update_image_dimensions(grk_image* p_image, uint32_t reduce)
+bool update_image_dimensions(grk_image* image, uint32_t reduce)
 {
-    uint32_t it_comp;
-    int32_t l_comp_x1, l_comp_y1;
-    auto l_img_comp = p_image->comps;
-    for (it_comp = 0; it_comp < p_image->numcomps; ++it_comp) {
-        int32_t l_h, l_w;
-        if (p_image->x0 > (uint32_t)INT_MAX ||
-                p_image->y0 > (uint32_t)INT_MAX ||
-                p_image->x1 > (uint32_t)INT_MAX ||
-                p_image->y1 > (uint32_t)INT_MAX) {
+    for (uint32_t compno = 0; compno < image->numcomps; ++compno) {
+        auto img_comp = image->comps + compno;
+
+        if (image->x0 > (uint32_t)INT_MAX ||
+                image->y0 > (uint32_t)INT_MAX ||
+                image->x1 > (uint32_t)INT_MAX ||
+                image->y1 > (uint32_t)INT_MAX) {
             GROK_ERROR("Image coordinates above INT_MAX are not supported\n");
             return false;
         }
 
-        l_img_comp->x0 = (uint32_t)ceildiv<int32_t>((int32_t)p_image->x0,
-                         (int32_t)l_img_comp->dx);
-        l_img_comp->y0 = (uint32_t)ceildiv<int32_t>((int32_t)p_image->y0,
-                         (int32_t)l_img_comp->dy);
-        l_comp_x1 = ceildiv<int32_t>((int32_t)p_image->x1, (int32_t)l_img_comp->dx);
-        l_comp_y1 = ceildiv<int32_t>((int32_t)p_image->y1, (int32_t)l_img_comp->dy);
+        img_comp->x0 = (uint32_t)ceildiv<int32_t>((int32_t)image->x0,
+                         (int32_t)img_comp->dx);
+        img_comp->y0 = (uint32_t)ceildiv<int32_t>((int32_t)image->y0,
+                         (int32_t)img_comp->dy);
+        int32_t comp_x1 = ceildiv<int32_t>((int32_t)image->x1, (int32_t)img_comp->dx);
+        int32_t comp_y1 = ceildiv<int32_t>((int32_t)image->y1, (int32_t)img_comp->dy);
 
-        l_w = int_ceildivpow2(l_comp_x1, (int32_t)reduce)
-              - int_ceildivpow2((int32_t)l_img_comp->x0, (int32_t)reduce);
-        if (l_w < 0) {
+        int32_t w = int_ceildivpow2(comp_x1, (int32_t)reduce)
+              - int_ceildivpow2((int32_t)img_comp->x0, (int32_t)reduce);
+        if (w < 0) {
             GROK_ERROR("Size x of the decoded component image is incorrect (comp[%d].w=%d).\n",
-                          it_comp, l_w);
+                          compno, w);
             return false;
         }
-        l_img_comp->w = (uint32_t)l_w;
-        l_h = int_ceildivpow2(l_comp_y1, (int32_t)reduce)
-              - int_ceildivpow2((int32_t)l_img_comp->y0, (int32_t)reduce);
-        if (l_h < 0) {
+        img_comp->w = (uint32_t)w;
+        int32_t h = int_ceildivpow2(comp_y1, (int32_t)reduce)
+              - int_ceildivpow2((int32_t)img_comp->y0, (int32_t)reduce);
+        if (h < 0) {
             GROK_ERROR("Size y of the decoded component image is incorrect (comp[%d].h=%d).\n",
-                          it_comp, l_h);
+                          compno, h);
             return false;
         }
-        l_img_comp->h = (uint32_t)l_h;
-        l_img_comp++;
+        img_comp->h = (uint32_t)h;
     }
 
     return true;
@@ -253,14 +251,14 @@ bool update_image_dimensions(grk_image* p_image, uint32_t reduce)
  Assumption:  src and dest have the same number of components
  */
 void transfer_image_data(grk_image *src, grk_image *dest) {
-	uint32_t compno;
 	if (!src || !dest || !src->comps || !dest->comps
 			|| src->numcomps != dest->numcomps)
 		return;
 
-	for (compno = 0; compno < src->numcomps; compno++) {
-		 grk_image_comp  *src_comp = src->comps + compno;
-		 grk_image_comp  *dest_comp = dest->comps + compno;
+	for (uint32_t compno = 0; compno < src->numcomps; compno++) {
+		auto src_comp = src->comps + compno;
+		auto dest_comp = dest->comps + compno;
+
 		dest_comp->resno_decoded = src_comp->resno_decoded;
 		grk_image_single_component_data_free(dest_comp);
 		dest_comp->data = src_comp->data;
