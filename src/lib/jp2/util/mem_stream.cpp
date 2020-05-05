95cd6f27f2733096791092c6449772db82c09a1e
Aaron Boxer <boxerab@gmail.com>
2020-05-05 18:19:05 -0400

    mem mapped file: improve error handling

--- src/lib/jp2/util/mem_stream.cpp
+++ src/lib/jp2/util/mem_stream.cpp
@@ -235,18 +235,18 @@ static grk_handle open_fd(const char* fname, const char* mode)
                                    FILE_SHARE_READ | FILE_SHARE_WRITE, nullptr, dwMode,
                                    (m == O_RDONLY) ? FILE_ATTRIBUTE_READONLY : FILE_ATTRIBUTE_NORMAL,
                                    nullptr);
-    if (fd == INVALID_HANDLE_VALUE) {
+    if (fd == INVALID_HANDLE_VALUE)
         return (grk_handle)-1;
-    }
+
     return fd;
 }
 
 static int32_t close_fd(grk_handle fd)
 {
     int32_t rc = -1;
-    if (fd) {
+    if (fd)
         rc = CloseHandle(fd) ? 0 : -1;
-    }
+
     return rc;
 }
 
@@ -277,27 +277,28 @@ static void* grk_map(grk_handle fd, size_t len) {
 }
 
 static int32_t unmap(void *ptr, size_t len) {
+	int32_t rc = -1;
 	if (ptr)
-		munmap(ptr, len);
-	return 0;
+		rc = munmap(ptr, len);
+	return rc;
 }
 
 static grk_handle open_fd(const char *fname, const char *mode) {
 	grk_handle fd = 0;
 	int32_t m = -1;
-	if (!fname) {
+	if (!fname)
 		return (grk_handle) -1;
-	}
 	m = get_file_open_mode(mode);
+	if (m == -1)
+		return (grk_handle) -1;
 	fd = open(fname, m, 0666);
 	if (fd < 0) {
-#ifdef DEBUG_ERRNO
-        if (errno > 0 && strerror(errno) != nullptr) {
-            printf("%s: %s", fname, strerror(errno));
-        } else {
-            printf("%s: Cannot open", fname);
+        if (errno > 0 && strerror(errno) != nullptr){
+            GROK_ERROR("%s: %s", fname, strerror(errno));
         }
-#endif
+        else {
+        	GROK_ERROR("%s: Cannot open", fname);
+        }
 		return (grk_handle) -1;
 	}
 	return fd;
@@ -306,7 +307,7 @@ static grk_handle open_fd(const char *fname, const char *mode) {
 static int32_t close_fd(grk_handle fd) {
 	if (!fd)
 		return 0;
-	return (close(fd));
+	return close(fd);
 }
 
 #endif
@@ -314,8 +315,12 @@ static int32_t close_fd(grk_handle fd) {
 static void mem_map_free(void *user_data) {
 	if (user_data) {
 		buf_info *buffer_info = (buf_info*) user_data;
-		unmap(buffer_info->buf, buffer_info->len);
-		close_fd(buffer_info->fd);
+		int32_t rc = unmap(buffer_info->buf, buffer_info->len);
+		if (rc)
+			GROK_ERROR("Unmapping memory mapped file failed with error %d", rc);
+		rc = close_fd(buffer_info->fd);
+		if (rc)
+			GROK_ERROR("Closing memory mapped file failed with error %d", rc);
 		delete buffer_info;
 	}
 }
@@ -327,14 +332,17 @@ grk_stream* create_mapped_file_read_stream(const char *fname) {
 	bool is_read_stream = true;
 
 	grk_handle fd = open_fd(fname, is_read_stream ? "r" : "w");
-	if (fd == (grk_handle) -1)
+	if (fd == (grk_handle) -1){
+		GROK_ERROR("Unable to open memory mapped file %s", fname);
 		return nullptr;
+	}
 
 	auto buffer_info = new buf_info();
 	buffer_info->fd = fd;
 	buffer_info->len = (size_t) size_proc(fd);
 	auto mapped_view = grk_map(fd, buffer_info->len);
 	if (!mapped_view) {
+		GROK_ERROR("Unable to map memory mapped file %s", fname);
 		mem_map_free(buffer_info);
 		return nullptr;
 	}
