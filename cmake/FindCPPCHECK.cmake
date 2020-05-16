5a3c3e5a4aa6574e6dd18f574193375ab5dce68f
Antonio Ospite <ao2@ao2.it>
2020-05-16 23:32:15 +0200

    cmake/FindCPPCHECK.cmake: enforce the REQUIRED flag passed to find_package

--- cmake/FindCPPCHECK.cmake
+++ cmake/FindCPPCHECK.cmake
@@ -1,2 +1,8 @@
 find_program(CPPCHECK_EXECUTABLE cppcheck )
 mark_as_advanced( CPPCHECK_EXECUTABLE  )
+
+if (NOT CPPCHECK_EXECUTABLE)
+  if (CPPCHECK_FIND_REQUIRED)
+    message(FATAL_ERROR "ERROR: Could not find cppcheck")
+  endif ()
+endif ()
