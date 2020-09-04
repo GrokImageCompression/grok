f9219e6d05e01f8e1018962a8be94debbfdbc7f1
Aaron Boxer <boxerab@gmail.com>
2020-09-03 22:06:07 -0400

    mct: integrate shift and clamp in reversible mct decode

--- src/lib/jp2/point_transform/mct.cpp
+++ src/lib/jp2/point_transform/mct.cpp
@@ -106,164 +106,6 @@ void mct::encode_rev(int32_t *GRK_RESTRICT chan0, int32_t *GRK_RESTRICT chan1,
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/* <summary> */
-/* Inverse reversible MCT. */
-/* </summary> */
-void mct::decode_rev(grk_tile *tile, grk_image *image,TileComponentCodingParams *tccps) {
-	size_t i = 0;
-	int32_t *GRK_RESTRICT chan0 = tile->comps[0].buf->ptr();
-	int32_t *GRK_RESTRICT chan1 = tile->comps[1].buf->ptr();
-	int32_t *GRK_RESTRICT chan2 = tile->comps[2].buf->ptr();
-
-	int32_t _min[3];
-    int32_t _max[3];
-	int32_t shift[3];
-    for (uint32_t compno =0; compno < 3; ++compno) {
-    	auto img_comp = image->comps + compno;
-		if (img_comp->sgnd) {
-			_min[compno] = -(1 << (img_comp->prec - 1));
-			_max[compno] = (1 << (img_comp->prec - 1)) - 1;
-		} else {
-			_min[compno] = 0;
-			_max[compno] = (1 << img_comp->prec) - 1;
-		}
-    	auto tccp = tccps + compno;
-    	shift[compno] = tccp->m_dc_level_shift;
-    }
-
-	uint64_t n = tile->comps->buf->strided_area();
-
-	if (CPUArch::SSE2() || CPUArch::AVX2() ) {
-#if (defined(__SSE2__) || defined(__AVX2__))
-	size_t num_threads = ThreadPool::get()->num_threads();
-    size_t chunkSize = n / num_threads;
-    //ensure it is divisible by VREG_INT_COUNT
-    chunkSize = (chunkSize/VREG_INT_COUNT) * VREG_INT_COUNT;
-	if (chunkSize > VREG_INT_COUNT) {
-	    std::vector< std::future<int> > results;
-	    for(uint64_t threadid = 0; threadid < num_threads; ++threadid) {
-	    	uint64_t index = threadid;
-	    	auto decoder = [index, chunkSize,chan0,chan1,chan2](){
-	    		uint64_t begin = (uint64_t)index * chunkSize;
-				for (auto j = begin; j < begin+chunkSize; j+=VREG_INT_COUNT ){
-					VREG y = LOAD(chan0 + j);
-					VREG u = LOAD(chan1 + j);
-					VREG v = LOAD(chan2 + j);
-					VREG g = SUB(y, SAR(ADD(u, v), 2));
-					VREG r = ADD(v, g);
-					VREG b = ADD(u, g);
-					STORE(chan0 + j, r);
-					STORE(chan1 + j, g);
-					STORE(chan2 + j, b);
-				}
-				return 0;
-	    	};
-
-	    	if (num_threads > 1)
-	    		results.emplace_back(ThreadPool::get()->enqueue(decoder));
-	    	else
-	    		decoder();
-
-	    }
-	    for(auto &result: results){
-	        result.get();
-	    }
-		i = chunkSize * num_threads;
-	}
-#endif
-	}
-	for (; i < n; ++i) {
-		int32_t y = chan0[i];
-		int32_t u = chan1[i];
-		int32_t v = chan2[i];
-		int32_t g = y - ((u + v) >> 2);
-		int32_t r = v + g;
-		int32_t b = u + g;
-		chan0[i] = r;
-		chan1[i] = g;
-		chan2[i] = b;
-	}
-}
-/* <summary> */
-/* Forward irreversible MCT. */
-/* </summary> */
-void mct::encode_irrev( int* GRK_RESTRICT chan0,
-		int* GRK_RESTRICT chan1,
-		int* GRK_RESTRICT chan2,
-						uint64_t n)
-{
-    size_t i = 0;
-
-    const float a_r = 0.299f;
-    const float a_g = 0.587f;
-    const float a_b = 0.114f;
-    const float cb = 0.5f/(1.0f-a_b);
-    const float cr = 0.5f/(1.0f-a_r);
-
-	if (CPUArch::AVX2() ) {
-#if ( defined(__AVX2__))
-	size_t num_threads = ThreadPool::get()->num_threads();
-    size_t chunkSize = n / num_threads;
-    //ensure it is divisible by VREG_INT_COUNT
-    chunkSize = (chunkSize/VREG_INT_COUNT) * VREG_INT_COUNT;
-	if (chunkSize > VREG_INT_COUNT) {
-		std::vector< std::future<int> > results;
-	    for(uint64_t tr = 0; tr < num_threads; ++tr) {
-	    	uint64_t index = tr;
-			auto encoder = [index, chunkSize, chan0,chan1,chan2]()	{
-				const VREGF va_r = LOAD_CST_F(0.299f);
-				const VREGF va_g = LOAD_CST_F(0.587f);
-				const VREGF va_b = LOAD_CST_F(0.114f);
-				const VREGF vcb = LOAD_CST_F(0.5f/(1.0f-0.114f));
-				const VREGF vcr = LOAD_CST_F(0.5f/(1.0f-0.299f));
-
-				uint64_t begin = (uint64_t)index * chunkSize;
-				for (auto j = begin; j < begin+chunkSize; j+=VREG_INT_COUNT ){
-					VREG ri = LOAD(chan0 + j);
-					VREG gi = LOAD(chan1 + j);
-					VREG bi = LOAD(chan2 + j);
-
-					VREGF r = _mm256_cvtepi32_ps(ri);
-					VREGF g = _mm256_cvtepi32_ps(gi);
-					VREGF b = _mm256_cvtepi32_ps(bi);
-
-					VREGF y = ADDF(ADDF(MULF(r, va_r),MULF(g, va_g)),MULF(b, va_b)) ;
-					VREGF u = MULF(vcb, SUBF(b, y));
-					VREGF v = MULF(vcr, SUBF(r, y));
-
-					STORE(chan0 + j, _mm256_cvttps_epi32(y * (1 << 11)));
-					STORE(chan1 + j, _mm256_cvttps_epi32(u * (1 << 11)));
-					STORE(chan2 + j, _mm256_cvttps_epi32(v * (1 << 11)));
-				}
-				return 0;
-			};
-
-			if (num_threads > 1)
-				results.emplace_back(ThreadPool::get()->enqueue(encoder));
-			else
-				encoder();
-		}
-		for(auto &result: results){
-			result.get();
-		}
-		i = num_threads * chunkSize;
-	}
-#endif
-    }
-    for(; i < n; ++i) {
-        float r = (float)chan0[i];
-        float g = (float)chan1[i];
-        float b = (float)chan2[i];
-
-        float y = a_r * r + a_g * g + a_b * b;
-        float u = cb * (b - y);
-        float v = cr * (r - y);
-
-        chan0[i] = (int32_t)(y * (1 << 11));
-        chan1[i] = (int32_t)(u * (1 << 11));
-        chan2[i] = (int32_t)(v * (1 << 11));
-    }
-}
 
 /* <summary> */
 /* Inverse irreversible MCT. */
@@ -363,7 +205,176 @@ void mct::decode_irrev(grk_tile *tile, grk_image *image,TileComponentCodingParam
 	}
 }
 
-//////////////////////////////////////////////////////////////////////////////
+
+
+/* <summary> */
+/* Inverse reversible MCT. */
+/* </summary> */
+void mct::decode_rev(grk_tile *tile, grk_image *image,TileComponentCodingParams *tccps) {
+	size_t i = 0;
+	int32_t *GRK_RESTRICT c0 = tile->comps[0].buf->ptr();
+	int32_t *GRK_RESTRICT c1 = tile->comps[1].buf->ptr();
+	int32_t *GRK_RESTRICT c2 = tile->comps[2].buf->ptr();
+
+	int32_t _min[3];
+    int32_t _max[3];
+	int32_t shift[3];
+    for (uint32_t compno =0; compno < 3; ++compno) {
+    	auto img_comp = image->comps + compno;
+		if (img_comp->sgnd) {
+			_min[compno] = -(1 << (img_comp->prec - 1));
+			_max[compno] = (1 << (img_comp->prec - 1)) - 1;
+		} else {
+			_min[compno] = 0;
+			_max[compno] = (1 << img_comp->prec) - 1;
+		}
+    	auto tccp = tccps + compno;
+    	shift[compno] = tccp->m_dc_level_shift;
+    }
+
+	uint64_t n = tile->comps->buf->strided_area();
+
+	if (CPUArch::SSE2() || CPUArch::AVX2() ) {
+#if (defined(__SSE2__) || defined(__AVX2__))
+	size_t num_threads = ThreadPool::get()->num_threads();
+    size_t chunkSize = n / num_threads;
+    //ensure it is divisible by VREG_INT_COUNT
+    chunkSize = (chunkSize/VREG_INT_COUNT) * VREG_INT_COUNT;
+	if (chunkSize > VREG_INT_COUNT) {
+	    std::vector< std::future<int> > results;
+	    for(uint64_t threadid = 0; threadid < num_threads; ++threadid) {
+	    	uint64_t index = threadid;
+	    	auto decoder = [index, chunkSize,c0,c1,c2, &shift, &_min, &_max](){
+	    		uint64_t begin = (uint64_t)index * chunkSize;
+				const VREG  vdcr = LOAD_CST(shift[0]);
+				const VREG  vdcg = LOAD_CST(shift[1]);
+				const VREG  vdcb = LOAD_CST(shift[2]);
+				const VREG  minr = LOAD_CST(_min[0]);
+				const VREG  ming = LOAD_CST(_min[1]);
+				const VREG  minb = LOAD_CST(_min[2]);
+				const VREG  maxr = LOAD_CST(_max[0]);
+				const VREG  maxg = LOAD_CST(_max[1]);
+				const VREG  maxb = LOAD_CST(_max[2]);
+				for (auto j = begin; j < begin+chunkSize; j+=VREG_INT_COUNT ){
+					VREG y = LOAD(c0 + j);
+					VREG u = LOAD(c1 + j);
+					VREG v = LOAD(c2 + j);
+					VREG g = SUB(y, SAR(ADD(u, v), 2));
+					VREG r = ADD(v, g);
+					VREG b = ADD(u, g);
+					STORE(c0 + j, VCLAMP(ADD(r,vdcr), minr, maxr));
+					STORE(c1 + j, VCLAMP(ADD(g,vdcg), ming, maxg));
+					STORE(c2 + j, VCLAMP(ADD(b,vdcb), minb, maxb));
+				}
+				return 0;
+	    	};
+
+	    	if (num_threads > 1)
+	    		results.emplace_back(ThreadPool::get()->enqueue(decoder));
+	    	else
+	    		decoder();
+
+	    }
+	    for(auto &result: results){
+	        result.get();
+	    }
+		i = chunkSize * num_threads;
+	}
+#endif
+	}
+	for (; i < n; ++i) {
+		int32_t y = c0[i];
+		int32_t u = c1[i];
+		int32_t v = c2[i];
+		int32_t g = y - ((u + v) >> 2);
+		int32_t r = v + g;
+		int32_t b = u + g;
+		c0[i] = std::clamp<int32_t>(r + shift[0], _min[0], _max[0]);
+		c1[i] = std::clamp<int32_t>(g + shift[1], _min[1], _max[1]);
+		c2[i] = std::clamp<int32_t>(b + shift[2], _min[2], _max[2]);
+	}
+}
+/* <summary> */
+/* Forward irreversible MCT. */
+/* </summary> */
+void mct::encode_irrev( int* GRK_RESTRICT chan0,
+		int* GRK_RESTRICT chan1,
+		int* GRK_RESTRICT chan2,
+						uint64_t n)
+{
+    size_t i = 0;
+
+    const float a_r = 0.299f;
+    const float a_g = 0.587f;
+    const float a_b = 0.114f;
+    const float cb = 0.5f/(1.0f-a_b);
+    const float cr = 0.5f/(1.0f-a_r);
+
+	if (CPUArch::AVX2() ) {
+#if ( defined(__AVX2__))
+	size_t num_threads = ThreadPool::get()->num_threads();
+    size_t chunkSize = n / num_threads;
+    //ensure it is divisible by VREG_INT_COUNT
+    chunkSize = (chunkSize/VREG_INT_COUNT) * VREG_INT_COUNT;
+	if (chunkSize > VREG_INT_COUNT) {
+		std::vector< std::future<int> > results;
+	    for(uint64_t tr = 0; tr < num_threads; ++tr) {
+	    	uint64_t index = tr;
+			auto encoder = [index, chunkSize, chan0,chan1,chan2]()	{
+				const VREGF va_r = LOAD_CST_F(0.299f);
+				const VREGF va_g = LOAD_CST_F(0.587f);
+				const VREGF va_b = LOAD_CST_F(0.114f);
+				const VREGF vcb = LOAD_CST_F(0.5f/(1.0f-0.114f));
+				const VREGF vcr = LOAD_CST_F(0.5f/(1.0f-0.299f));
+
+				uint64_t begin = (uint64_t)index * chunkSize;
+				for (auto j = begin; j < begin+chunkSize; j+=VREG_INT_COUNT ){
+					VREG ri = LOAD(chan0 + j);
+					VREG gi = LOAD(chan1 + j);
+					VREG bi = LOAD(chan2 + j);
+
+					VREGF r = _mm256_cvtepi32_ps(ri);
+					VREGF g = _mm256_cvtepi32_ps(gi);
+					VREGF b = _mm256_cvtepi32_ps(bi);
+
+					VREGF y = ADDF(ADDF(MULF(r, va_r),MULF(g, va_g)),MULF(b, va_b)) ;
+					VREGF u = MULF(vcb, SUBF(b, y));
+					VREGF v = MULF(vcr, SUBF(r, y));
+
+					STORE(chan0 + j, _mm256_cvttps_epi32(y * (1 << 11)));
+					STORE(chan1 + j, _mm256_cvttps_epi32(u * (1 << 11)));
+					STORE(chan2 + j, _mm256_cvttps_epi32(v * (1 << 11)));
+				}
+				return 0;
+			};
+
+			if (num_threads > 1)
+				results.emplace_back(ThreadPool::get()->enqueue(encoder));
+			else
+				encoder();
+		}
+		for(auto &result: results){
+			result.get();
+		}
+		i = num_threads * chunkSize;
+	}
+#endif
+    }
+    for(; i < n; ++i) {
+        float r = (float)chan0[i];
+        float g = (float)chan1[i];
+        float b = (float)chan2[i];
+
+        float y = a_r * r + a_g * g + a_b * b;
+        float u = cb * (b - y);
+        float v = cr * (r - y);
+
+        chan0[i] = (int32_t)(y * (1 << 11));
+        chan1[i] = (int32_t)(u * (1 << 11));
+        chan2[i] = (int32_t)(v * (1 << 11));
+    }
+}
+
 
 
 void mct::calculate_norms(double *pNorms, uint32_t pNbComps, float *pMatrix) {
