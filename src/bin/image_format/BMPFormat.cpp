3938c13213f682da57cbd333e73245309456a5d0
Aaron Boxer <boxerab@gmail.com>
2020-09-15 18:51:44 -0400

    add taskflow library: https://github.com/taskflow/taskflow

--- src/bin/image_format/BMPFormat.cpp
+++ src/bin/image_format/BMPFormat.cpp
@@ -26,7 +26,7 @@
 #include "convert.h"
 #include <cstring>
 #include "common.h"
-#include "ThreadPool.hpp"
+#include <taskflow/taskflow.hpp>
 
 // `MBED` in big endian format
 const uint32_t BMP_ICC_PROFILE_EMBEDDED = 0x4d424544;
@@ -1165,6 +1165,28 @@ bool BMPFormat::encodeHeader(grk_image *  image, const std::string &filename, ui
 }
 
 bool BMPFormat::encodeStrip(uint32_t rows){
+
+/*
+	  tf::Executor executor;
+	  tf::Taskflow taskflow;
+
+	  auto [A, B, C, D] = taskflow.emplace(
+	    [] () { std::cout << "TaskA\n"; },               //  task dependency graph
+	    [] () { std::cout << "TaskB\n"; },               //
+	    [] () { std::cout << "TaskC\n"; },               //          +---+
+	    [] () { std::cout << "TaskD\n"; }                //    +---->| B |-----+
+	  );                                                 //    |     +---+     |
+	                                                     //  +---+           +-v-+
+	  A.precede(B);  // A runs before B                  //  | A |           | D |
+	  A.precede(C);  // A runs before C                  //  +---+           +-^-+
+	  B.precede(D);  // B runs before D                  //    |     +---+     |
+	  C.precede(D);  // C runs before D                  //    +---->| C |-----+
+	                                                     //          +---+
+	  executor.run(taskflow).wait();
+*/
+
+
+
 	bool ret = false;
 	auto w = m_image->comps[0].w;
 	auto h = m_image->comps[0].h;
