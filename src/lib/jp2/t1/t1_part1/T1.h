b20a19711166c1201a95f6b03e9b772a7f9a0562
Aaron Boxer <boxerab@gmail.com>
2020-10-15 23:18:01 -0400

    refactor V

--- src/lib/jp2/t1/t1_part1/T1.h
+++ src/lib/jp2/t1/t1_part1/T1.h
@@ -21,9 +21,10 @@
 
 #pragma once
 
-#include "t1_flags.h"
-
 namespace grk {
+
+/** Flags for 4 consecutive rows of a column */
+typedef uint32_t grk_flag;
 
 #define T1_NUMCTXS_ZC  9
 #define T1_NUMCTXS_SC  5
@@ -38,97 +39,6 @@ namespace grk {
 #define T1_CTXNO_UNI (T1_CTXNO_AGG+T1_NUMCTXS_AGG)
 #define T1_NUMCTXS   (T1_CTXNO_UNI+T1_NUMCTXS_UNI)
 
-
-/** Flags for 4 consecutive rows of a column */
-typedef uint32_t grk_flag;
-
-
-/* BEGINNING of flags that apply to grk_flag */
-/** We hold the state of individual data points for the T1 encoder using
- *  a single 32-bit flags word to hold the state of 4 data points.  This corresponds
- *  to the 4-point-high columns that the data is processed in.
- *
- *  These \#defines declare the layout of a 32-bit flags word.
- *
- *  This is currently done for encoding only.
- *  The values must NOT be changed, otherwise this is going to break a lot of
- *  assumptions.
- */
-
-/* SIGMA: significance state (3 cols x 6 rows)
- * CHI:   state for negative sample value (1 col x 6 rows)
- * MU:    state for visited in refinement pass (1 col x 4 rows)
- * PI:    state for visited in significance pass (1 col * 4 rows)
- */
-
-#define T1_SIGMA_0  (1U << 0)
-#define T1_SIGMA_1  (1U << 1)
-#define T1_SIGMA_2  (1U << 2)
-#define T1_SIGMA_3  (1U << 3)
-#define T1_SIGMA_4  (1U << 4)
-#define T1_SIGMA_5  (1U << 5)
-#define T1_SIGMA_6  (1U << 6)
-#define T1_SIGMA_7  (1U << 7)
-#define T1_SIGMA_8  (1U << 8)
-#define T1_SIGMA_9  (1U << 9)
-#define T1_SIGMA_10 (1U << 10)
-#define T1_SIGMA_11 (1U << 11)
-#define T1_SIGMA_12 (1U << 12)
-#define T1_SIGMA_13 (1U << 13)
-#define T1_SIGMA_14 (1U << 14)
-#define T1_SIGMA_15 (1U << 15)
-#define T1_SIGMA_16 (1U << 16)
-#define T1_SIGMA_17 (1U << 17)
-
-
-/** As an example, the bits T1_SIGMA_3, T1_SIGMA_4 and T1_SIGMA_5
- *  indicate the significance state of the west neighbour of data point zero
- *  of our four, the point itself, and its east neighbour respectively.
- *  Many of the bits are arranged so that given a flags word, you can
- *  look at the values for the data point 0, then shift the flags
- *  word right by 3 bits and look at the same bit positions to see the
- *  values for data point 1.
- *
- *  The \#defines below help a bit with this; say you have a flags word
- *  f, you can do things like
- *
- *  (f & T1_SIGMA_THIS)
- *
- *  to see the significance bit of data point 0, then do
- *
- *  ((f >> 3) & T1_SIGMA_THIS)
- *
- *  to see the significance bit of data point 1.
- */
-
-#define T1_SIGMA_NW   T1_SIGMA_0
-#define T1_SIGMA_N    T1_SIGMA_1
-#define T1_SIGMA_NE   T1_SIGMA_2
-#define T1_SIGMA_W    T1_SIGMA_3
-#define T1_SIGMA_THIS T1_SIGMA_4
-#define T1_SIGMA_E    T1_SIGMA_5
-#define T1_SIGMA_SW   T1_SIGMA_6
-#define T1_SIGMA_S    T1_SIGMA_7
-#define T1_SIGMA_SE   T1_SIGMA_8
-#define T1_SIGMA_NEIGHBOURS (T1_SIGMA_NW | T1_SIGMA_N | T1_SIGMA_NE | T1_SIGMA_W | T1_SIGMA_E | T1_SIGMA_SW | T1_SIGMA_S | T1_SIGMA_SE)
-
-#define T1_CHI_THIS   T1_CHI_1
-#define T1_CHI_THIS_I T1_CHI_1_I
-#define T1_MU_THIS    T1_MU_0
-#define T1_PI_THIS    T1_PI_0
-#define T1_CHI_S      T1_CHI_2
-
-#define T1_LUT_SGN_W (1U << 0)
-#define T1_LUT_SIG_N (1U << 1)
-#define T1_LUT_SGN_E (1U << 2)
-#define T1_LUT_SIG_W (1U << 3)
-#define T1_LUT_SGN_N (1U << 4)
-#define T1_LUT_SIG_E (1U << 5)
-#define T1_LUT_SGN_S (1U << 6)
-#define T1_LUT_SIG_S (1U << 7)
-
-
-/* ----------------------------------------------------------------------- */
 
 struct T1 {
 
