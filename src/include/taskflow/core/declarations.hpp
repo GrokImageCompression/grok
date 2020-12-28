90e7829e66f33cb73c7fcd97675ac3474c7c0e7f
Aaron Boxer <boxerab@gmail.com>
2020-12-28 12:43:30 -0500

    taskflow update from master

--- src/include/taskflow/core/declarations.hpp
+++ src/include/taskflow/core/declarations.hpp
@@ -2,31 +2,36 @@
 
 namespace tf {
 
-// ----------------------------------------------------------------------------
-// forward declarations
-// ----------------------------------------------------------------------------
-
 // taskflow
+class AsyncTopology;
 class Node;
 class Graph;
 class FlowBuilder;
+class Semaphore;
 class Subflow;
 class Task;
 class TaskView;
 class Taskflow;
 class Topology;
+class TopologyBase;
 class Executor;
 class WorkerView;
 class ObserverInterface;
 class ChromeTracingObserver;
 class TFProfObserver;
+class TFProfManager;
 
-// cudaflow
+template <typename T>
+class Future;
+
+// cudaFlow
 class cudaNode;
 class cudaGraph;
 class cudaTask;
 class cudaFlow;
-
+class cudaFlowCapturer;
+class cudaFlowCapturerBase;
+class cublasFlowCapturer;
 
 
 }  // end of namespace tf -----------------------------------------------------
