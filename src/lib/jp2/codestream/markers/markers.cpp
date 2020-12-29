723a3fccc51168e0c4412e1fdfeb4db1b8e2e538
Aaron Boxer <boxerab@gmail.com>
2020-12-29 10:25:33 -0500

    decompress: use correct upper bound for code block area

--- src/lib/jp2/codestream/markers/markers.cpp
+++ src/lib/jp2/codestream/markers/markers.cpp
@@ -2221,10 +2221,10 @@ bool j2k_read_SPCod_SPCoc(CodeStream *codeStream, uint32_t compno, uint8_t *p_he
 	grk_read<uint8_t>(current_ptr++, &tccp->cblkh);
 
 	if ( tccp->cblkw > 8 || tccp->cblkh > 8
-			|| (tccp->cblkw + tccp->cblkh) > 8 ) {
-		GRK_ERROR("Illegal code-block width/height exponents (%d, %d) found in COD/COC marker segment.\n"
-		"Code-block dimensions must be powers of 2, in the range 4-1024, whose product "
-		"lies in the range 16-4096.",tccp->cblkw + 2, tccp->cblkh + 2);
+			|| (tccp->cblkw + tccp->cblkh) > 10 ) {
+		GRK_ERROR("Illegal code-block width/height (2^%d, 2^%d) found in COD/COC marker segment.\n"
+		"Code-block dimensions must be powers of 2, must be in the range 4-1024, and their product must "
+		"lie in the range 16-4096.",(uint32_t)tccp->cblkw + 2, (uint32_t)tccp->cblkh + 2);
 		return false;
 	}
 
