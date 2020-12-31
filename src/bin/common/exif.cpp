8bf8d8411b5d3abc6c794ec1e723efd146859f38
Aaron Boxer <boxerab@gmail.com>
2020-12-31 10:48:22 -0500

    exiftool: use Perl sub-routine with passed arguments

--- src/bin/common/exif.cpp
+++ src/bin/common/exif.cpp
@@ -29,35 +29,42 @@
 
 namespace grk {
 
-
 void transferExifTags(std::string src, std::string dest){
 #ifdef GROK_HAVE_EXIFTOOL
     std::string script {R"x(
-						use Image::ExifTool qw(ImageInfo);
-						use strict;
-						use warnings;
-						my $srcFile = $ARGV[0];
-						my $outFile = $ARGV[1];
-						my $exifTool = new Image::ExifTool;
-						my $info = $exifTool->SetNewValuesFromFile($srcFile, 'all:all');
-						my $result = $exifTool->WriteInfo($outFile);
-    					)x"};
-
-    PerlInterpreter *my_perl = nullptr;
-    constexpr int NUM_ARGS = 5;
-    const char* embedding[NUM_ARGS] = { "", "-e", "0", src.c_str(), dest.c_str() };
-    my_perl = perl_alloc();
+		use Image::ExifTool qw(ImageInfo);
+		use strict;
+		use warnings;
+		sub transfer {
+			my $srcFile = $_[0];
+			my $outFile = $_[1];
+			my $exifTool = new Image::ExifTool;
+			my $info = $exifTool->SetNewValuesFromFile($srcFile, 'all:all');
+			my $result = $exifTool->WriteInfo($outFile);
+		}
+    )x"};
+    constexpr int NUM_ARGS = 3;
+    const char* embedding[NUM_ARGS] = { "", "-e", "0" };
+    PERL_SYS_INIT3(NULL,NULL,NULL);
+    auto my_perl = perl_alloc();
     perl_construct( my_perl );
-    perl_parse(my_perl, NULL, NUM_ARGS, (char**)embedding, NULL);
+    int res = perl_parse(my_perl, NULL, NUM_ARGS, (char**)embedding, NULL);
+    assert(!res);
+    (void)res;
     perl_run(my_perl);
     eval_pv(script.c_str(), TRUE);
+    char *args[] = {(char*)src.c_str(), (char*)dest.c_str(), NULL};
+    call_argv("transfer", G_DISCARD, args);
     perl_destruct(my_perl);
     perl_free(my_perl);
+    PERL_SYS_TERM();
 #else
     (void)src;
     (void)dest;
     spdlog::warn("ExifTool not available; unable to transfer Exif tags");
 #endif
 }
+
+
 
 }
