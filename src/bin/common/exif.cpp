1cfd30e24f10d08ee17141f16320296efc9b58ca
Aaron Boxer <boxerab@gmail.com>
2020-12-31 11:32:50 -0500

    exif: manage Perl interpreter in singleton class

--- src/bin/common/exif.cpp
+++ src/bin/common/exif.cpp
@@ -29,35 +29,56 @@
 
 namespace grk {
 
+#ifdef GROK_HAVE_EXIFTOOL
+class PerlInterp {
+public:
+	PerlInterp() : my_perl(nullptr) {
+		 std::string script {R"x(
+				use Image::ExifTool qw(ImageInfo);
+				use strict;
+				use warnings;
+				sub transfer {
+					my $srcFile = $_[0];
+					my $outFile = $_[1];
+					my $exifTool = new Image::ExifTool;
+					my $info = $exifTool->SetNewValuesFromFile($srcFile, 'all:all');
+					my $result = $exifTool->WriteInfo($outFile);
+				}
+		    )x"};
+	    constexpr int NUM_ARGS = 3;
+	    const char* embedding[NUM_ARGS] = { "", "-e", "0" };
+	    PERL_SYS_INIT3(NULL,NULL,NULL);
+	    auto my_perl = perl_alloc();
+	    perl_construct( my_perl );
+	    int res = perl_parse(my_perl, NULL, NUM_ARGS, (char**)embedding, NULL);
+	    assert(!res);
+	    (void)res;
+	    perl_run(my_perl);
+	    eval_pv(script.c_str(), TRUE);
+	}
+
+	~PerlInterp(){
+		perl_destruct(my_perl);
+		perl_free(my_perl);
+		PERL_SYS_TERM();
+	}
+	PerlInterpreter *my_perl;
+};
+
+class PerlScriptRunner{
+public:
+	static PerlInterp *instance(void){
+		static PerlInterp interp;
+		return &interp;
+	}
+};
+#endif
+
 void transferExifTags(std::string src, std::string dest){
 #ifdef GROK_HAVE_EXIFTOOL
-    std::string script {R"x(
-		use Image::ExifTool qw(ImageInfo);
-		use strict;
-		use warnings;
-		sub transfer {
-			my $srcFile = $_[0];
-			my $outFile = $_[1];
-			my $exifTool = new Image::ExifTool;
-			my $info = $exifTool->SetNewValuesFromFile($srcFile, 'all:all');
-			my $result = $exifTool->WriteInfo($outFile);
-		}
-    )x"};
-    constexpr int NUM_ARGS = 3;
-    const char* embedding[NUM_ARGS] = { "", "-e", "0" };
-    PERL_SYS_INIT3(NULL,NULL,NULL);
-    auto my_perl = perl_alloc();
-    perl_construct( my_perl );
-    int res = perl_parse(my_perl, NULL, NUM_ARGS, (char**)embedding, NULL);
-    assert(!res);
-    (void)res;
-    perl_run(my_perl);
-    eval_pv(script.c_str(), TRUE);
+	PerlScriptRunner::instance();
     char *args[] = {(char*)src.c_str(), (char*)dest.c_str(), NULL};
     call_argv("transfer", G_DISCARD, args);
-    perl_destruct(my_perl);
-    perl_free(my_perl);
-    PERL_SYS_TERM();
 #else
     (void)src;
     (void)dest;
