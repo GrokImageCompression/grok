c5083048e18b1eb817c37c3285d932ad132c6106
Aaron Boxer <boxerab@gmail.com>
2021-01-02 21:54:32 -0500

    license: udpate

--- tests/performance/bench.py
+++ tests/performance/bench.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+#    Copyright (C) 2016-2021 Grok Image Compression Inc.
+#
+#    This source code is free software: you can redistribute it and/or  modify
+#    it under the terms of the GNU Affero General Public License, version 3,
+#    as published by the Free Software Foundation.
+#
+#    This source code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import subprocess
 import sys
 import os
