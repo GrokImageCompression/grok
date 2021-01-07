1e74f60c6956ca392afcc6000b0f719bcef7c42e
Aaron Boxer <boxerab@gmail.com>
2021-01-07 11:57:27 -0500

    5/3 wavelet: revert to old filter padding

--- src/lib/jp2/transform/WaveletReverse.h
+++ src/lib/jp2/transform/WaveletReverse.h
@@ -43,7 +43,7 @@ struct  vec4f {
 uint32_t max_resolution(Resolution *GRK_RESTRICT r, uint32_t i);
 
 template<class T> constexpr T getFilterPad(bool lossless) {
-     return  lossless ? 3 : 5;
+     return  lossless ? 3 : 4;
  }
 
 template<class T> constexpr T getHorizontalPassHeight(bool lossless){
