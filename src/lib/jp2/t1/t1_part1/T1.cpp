0b06393a0601749420dad49d584238967a513a04
Aaron Boxer <boxerab@gmail.com>
2021-02-01 08:56:54 -0500

    rate control: restore old wavelet norm LUT

--- src/lib/jp2/t1/t1_part1/T1.cpp
+++ src/lib/jp2/t1/t1_part1/T1.cpp
@@ -239,8 +239,30 @@ static INLINE void update_flags(grk_flag *flagsp, uint32_t ci, uint32_t s,
 }
 
 
+static const double dwt_norms[4][10] = {
+    {1.000, 1.500, 2.750, 5.375, 10.68, 21.34, 42.67, 85.33, 170.7, 341.3},
+    {1.038, 1.592, 2.919, 5.703, 11.33, 22.64, 45.25, 90.48, 180.9},
+    {1.038, 1.592, 2.919, 5.703, 11.33, 22.64, 45.25, 90.48, 180.9},
+    {.7186, .9218, 1.586, 3.043, 6.019, 12.01, 24.00, 47.97, 95.93}
+};
+static const double dwt_norms_real[4][10] = {
+    {1.000, 1.965, 4.177, 8.403, 16.90, 33.84, 67.69, 135.3, 270.6, 540.9},
+    {2.022, 3.989, 8.355, 17.04, 34.27, 68.63, 137.3, 274.6, 549.0},
+    {2.022, 3.989, 8.355, 17.04, 34.27, 68.63, 137.3, 274.6, 549.0},
+    {2.080, 3.865, 8.307, 17.18, 34.71, 69.59, 139.3, 278.6, 557.2}
+};
+
+
 double T1::getnorm(uint32_t level, uint8_t orientation, bool reversible) {
-	assert(orientation <= 3);
+   assert(orientation <= 3);
+   if (orientation == 0 && level > 9) {
+        level = 9;
+   } else if (orientation > 0 && level > 8) {
+        level = 8;
+   }
+   return reversible ? dwt_norms[orientation][level] : dwt_norms_real[orientation][level];
+
+/*
 	switch(orientation){
 	case 0:
 		return sqrt_energy_gains::get_gain_l(level,reversible) *
@@ -257,7 +279,9 @@ double T1::getnorm(uint32_t level, uint8_t orientation, bool reversible) {
 		break;
 	default:
 		return 0;
+
 	}
+*/
 }
 
 
