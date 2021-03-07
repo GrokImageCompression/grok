1a40fab4fcec7f78a7cf9e2a3f0b040d93115051
Aaron Boxer <boxerab@gmail.com>
2021-03-07 10:56:07 -0500

    dump: plug memory leak - leaking stream

--- src/bin/jp2/grk_dump.cpp
+++ src/bin/jp2/grk_dump.cpp
@@ -421,7 +421,7 @@ int main(int argc, char *argv[]) {
 				continue;
 			}
 		}
-		auto stream = grk_stream_create_file_stream(parameters.infile, 1024 * 1024,1);
+		stream = grk_stream_create_file_stream(parameters.infile, 1024 * 1024,1);
 		if (!stream) {
 			spdlog::error("failed to create a stream from file {}",
 					parameters.infile);
