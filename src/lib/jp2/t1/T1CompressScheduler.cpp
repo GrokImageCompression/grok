568d1b2cec56a21a09bc273d946cc8ecffe185da
Aaron Boxer <boxerab@gmail.com>
2022-01-12 19:59:28 -0500

    compress: don't compress empty code blocks

--- src/lib/jp2/t1/T1CompressScheduler.cpp
+++ src/lib/jp2/t1/T1CompressScheduler.cpp
@@ -54,6 +54,8 @@ void T1CompressScheduler::scheduleCompress(TileCodingParams* tcp, const double* 
 					for(uint64_t cblkno = 0; cblkno < prc->getNumCblks(); ++cblkno)
 					{
 						auto cblk = prc->getCompressedBlockPtr(cblkno);
+						if (!cblk->non_empty())
+							continue;
 						if(!cblk->allocData(nominalBlockSize))
 							continue;
 						auto block = new CompressBlockExec();
