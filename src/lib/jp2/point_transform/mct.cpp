40974394fdcbee964b83b9caa9c4694545429051
Aaron Boxer <boxerab@gmail.com>
2022-04-18 20:20:46 -0400

    enable strip cache for single tile lossy

--- src/lib/jp2/point_transform/mct.cpp
+++ src/lib/jp2/point_transform/mct.cpp
@@ -61,8 +61,8 @@ namespace HWY_NAMESPACE
 				auto ni = Clamp(NearestInt(Load(df, chan0 + j)) + vshift, vmin, vmax);
 				Store(ni, di, (int32_t*)(chan0 + j));
 			}
-			//if (info.stripCache_->isInitialized() && !info.stripCache_->isMultiTile())
-			//	info.stripCache_->ingestStrip(info.tile, info.yBegin, info.yEnd);
+			if (info.stripCache_->isInitialized() && !info.stripCache_->isMultiTile())
+				info.stripCache_->ingestStrip(info.tile, info.yBegin, info.yEnd);
 		}
 	};
 
