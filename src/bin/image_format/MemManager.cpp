6cb7940d773e5d59a18bc83551e521c0b886eb00
Aaron Boxer <boxerab@protonmail.com>
2022-06-18 15:30:48 -0400

    use standard library aligned alloc (except on windoze)

--- src/bin/image_format/MemManager.cpp
+++ src/bin/image_format/MemManager.cpp
@@ -22,11 +22,8 @@
 #include "MemManager.h"
 #include <cstdio>
 
-// const bool debugMemory = false;
-
-// OSX is missing C++11 aligned_alloc (stdlib.h version)
-#if defined(__APPLE__)
-#undef GROK_HAVE_ALIGNED_ALLOC
+#ifdef _WIN32
+#include <malloc.h>
 #endif
 
 #ifndef SIZE_MAX
@@ -59,51 +56,12 @@ static inline void* grkAlignedAllocN(size_t alignment, size_t size)
 	// make new_size a multiple of alignment
 	size = ((size + alignment - 1) / alignment) * alignment;
 
-#if defined(GROK_HAVE_ALIGNED_ALLOC)
-	ptr = aligned_alloc(alignment, size);
-#elif defined(GROK_HAVE_POSIX_MEMALIGN)
-	/* aligned_alloc requires c11, restrict to posix_memalign for now. Quote:
-	 * This function was introduced in POSIX 1003.1d. Although this function is
-	 * superseded by aligned_alloc, it is more portable to older POSIX systems
-	 * that do not support ISO C11.  */
-	if(posix_memalign(&ptr, alignment, size))
-		ptr = nullptr;
-		/* older linux */
-#elif defined(GROK_HAVE_MEMALIGN)
-	ptr = memalign(alignment, size);
-	/* _MSC_VER */
-#elif defined(GROK_HAVE__ALIGNED_MALLOC)
-	ptr = _aligned_malloc(size, alignment);
+#ifdef _WIN32
+	ptr = _aligned_malloc(size,alignment);
 #else
-	/*
-	 * Generic aligned malloc implementation.
-	 * Uses size_t offset for the integer manipulation of the pointer,
-	 * as uintptr_t is not available in C89 to do
-	 * bitwise operations on the pointer itself.
-	 */
-	alignment--;
-	{
-		/* Room for padding and extra pointer stored in front of allocated area */
-		size_t overhead = alignment + sizeof(void*);
-
-		/* let's be extra careful */
-		assert(alignment <= (SIZE_MAX - sizeof(void*)));
-
-		/* Avoid integer overflow */
-		if(size > (SIZE_MAX - overhead))
-			return nullptr;
-
-		uint8_t* mem = (uint8_t*)malloc(size + overhead);
-		if(mem == nullptr)
-			return mem;
-		/* offset = ((alignment + 1U) - ((size_t)(mem + sizeof(void*)) & alignment)) & alignment; */
-		/* Use the fact that alignment + 1U is a power of 2 */
-		size_t offset =
-			((alignment ^ ((size_t)(mem + sizeof(void*)) & alignment)) + 1U) & alignment;
-		ptr = (void*)(mem + sizeof(void*) + offset);
-		((void**)ptr)[-1] = mem;
-	}
+	ptr = std::aligned_alloc(alignment, size);
 #endif
+
 	return ptr;
 }
 void* grk_aligned_malloc(size_t size)
@@ -112,17 +70,10 @@ void* grk_aligned_malloc(size_t size)
 }
 void grk_aligned_free(void* ptr)
 {
-	// if (debugMemory && ptr)
-	//	printf("Binary Aligned dealloc  %p\n", ptr);
-#if defined(GROK_HAVE_POSIX_MEMALIGN) || defined(GROK_HAVE_ALIGNED_ALLOC) || \
-	defined(GROK_HAVE_MEMALIGN)
-	free(ptr);
-#elif defined(GROK_HAVE__ALIGNED_MALLOC)
+#ifdef _WIN32
 	_aligned_free(ptr);
 #else
-	/* Generic implementation has malloced pointer stored in front of used area */
-	if(ptr != nullptr)
-		free(((void**)ptr)[-1]);
+	free(ptr);
 #endif
 }
 } // namespace grk_bin
