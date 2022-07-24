27541a262c8b601d62f387ce9cd68557abe01127
Aaron Boxer <boxerab@protonmail.com>
2022-07-24 19:34:56 -0400

    add examples

---
+++ src/examples/codec/grk_example_codec_decompress.cpp
@@ -0,0 +1,57 @@
+/*
+ *    Copyright (C) 2016-2022 Grok Image Compression Inc.
+ *
+ *    This source code is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This source code is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include "grok_codec.h"
+#include "grk_examples_config.h"
+#include <string>
+#include <cstring>
+#include <vector>
+
+const std::string dataRoot = GRK_DATA_ROOT;
+
+int main(int argc, char** argv)
+{
+	(void)argc;
+	(void)argv;
+
+	//1. form vector of command line args
+	std::vector<std::string> argString;
+	argString.push_back("grk_codec_decompress");
+	argString.push_back("-v");
+	std::string temp = "-i " + dataRoot + "/input/nonregression/boats_cprl.j2k";
+	argString.push_back(temp);
+	argString.push_back("-o boats_cprl.tif");
+
+	// 2. convert to character strings
+	std::vector<char *> args;
+	for (auto& s : argString){
+	  char *arg = new char[s.size() + 1];
+	  copy(s.begin(), s.end(), arg);
+	  arg[s.size()] = '\0';
+	  args.push_back(arg);
+	}
+
+	// 3. decompress
+	int rc =  grk_codec_decompress((int)args.size(),&args[0]);
+
+	//4. cleanup
+	for (auto& s : args)
+	  delete[] s;
+
+	return rc;
+}
