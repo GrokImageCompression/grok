bf73f35412bfbc9e3fb250594ea3bbb20da4c9dd
Grok Compression <support@grokcompression.com>
2025-10-09 22:27:23 -0400

    more tweaks

---
+++ src/lib/core/debug/debug_context.cpp
@@ -0,0 +1,134 @@
+/*
+ *    Copyright (C) 2016-2025 Grok Image Compression Inc.
+ *
+ *    This source code is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This source code is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "mqc.h"
+#include "debug_context.h"
+
+namespace grk
+{
+
+// LocationProbe member functions
+LocationProbe::LocationProbe(uint8_t passno, uint8_t position, uint16_t i, uint16_t k)
+    : passno_(passno), position_(position), i_(i), k_(k)
+{}
+
+bool LocationProbe::probe(uint8_t passno, uint8_t position, uint16_t i, uint16_t k) const
+{
+  return (passno == passno_ && position == position_ && i == i_ && k == k_);
+}
+
+DebugContext::DebugContext()
+    : compno_(0), resno_(0), precinctIndex_(0), layno_(0), cblkno_(0), numResolutions_(0),
+      differentialLayers_(0), debug_backup(false), maxLayers_(0)
+{}
+
+// DebugContext singleton instance retrieval
+DebugContext& DebugContext::getInstance()
+{
+  static DebugContext instance;
+  return instance;
+}
+
+// Handling the mqcoder
+bool DebugContext::handle(const mqcoder& mq, uint8_t passno, uint8_t position, uint16_t i,
+                          uint16_t k)
+{
+  uint16_t layer = static_cast<uint16_t>(mq.cur_buffer_index);
+  auto& queue = getQueue(layer);
+  if(!mq.cached_)
+  {
+    queue.push(mq);
+    logProbe(false, layer, queue.size(), mq.c, passno, position, i, k);
+    queueSize_[layer] = queue.size();
+    return true;
+  }
+  else if(!queue.empty())
+  {
+    if(mq.backup_->i != BACKUP_DISABLED)
+      return true;
+    mqcoder ref = queue.front();
+    queue.pop();
+    bool isEqual = (mq == ref);
+    logProbe(true, layer, queue.size(), ref.c, passno, position, i, k);
+    if(!isEqual)
+      printf("Not equal at passno=%d,position=%d,i=%d,k=%d; ref=0x%x,diff=0x%x\n", passno, position,
+             i, k, ref.c, mq.c);
+    assert(isEqual);
+    return isEqual;
+  }
+  else
+  {
+    printf("Warning: reference coders are empty\n");
+    return true;
+  }
+}
+
+void DebugContext::restoreBackup(void)
+{
+  while(!backupQueue_.empty())
+  {
+    auto& c = backupQueue_.front();
+    assert(c.coder_.cached_);
+    handle(c.coder_, c.passno_, c.position_, c.i_, c.k_);
+    backupQueue_.pop();
+  }
+}
+
+// Incrementing differential layer
+void DebugContext::incrementDifferentialLayer()
+{
+  if(debug_backup)
+  {
+    if(++differentialLayers_ == maxLayers_)
+    {
+      checkEmpty();
+      ++cblkno_;
+      differentialLayers_ = 0;
+    }
+  }
+}
+
+// Checking if all queues are empty
+void DebugContext::checkEmpty() const
+{
+  for(const auto& [key, queue] : referenceCoders_)
+  {
+    std::ignore = key;
+    assert(queue.empty());
+  }
+}
+
+// Getting the queue for a specific layer
+std::queue<mqcoder>& DebugContext::getQueue(uint16_t layer)
+{
+  return referenceCoders_[{resno_, layer}];
+}
+
+// Logging probe results
+void DebugContext::logProbe(bool differential, uint16_t layer, size_t order, uint32_t c,
+                            uint8_t passno, uint8_t position, uint16_t i, uint16_t k) const
+{
+  if(probe_.probe(passno, position, i, k))
+  {
+    const char* action = differential ? "popped differential" : "pushed full";
+    order = differential ? queueSize_[layer] - order : order;
+    printf("%s -> layer: %d, position: %d, order: %zu, i=%d,k=%d,c: 0x%x\n", action, layer,
+           position, order, i, k, c);
+  }
+}
+
+} // namespace grk
---
+++ src/lib/core/debug/debug_context.h
@@ -0,0 +1,88 @@
+/*
+ *    Copyright (C) 2016-2025 Grok Image Compression Inc.
+ *
+ *    This source code is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This source code is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+#include <queue>
+#include <map>
+#include <cassert>
+#include <cstdint>
+
+namespace grk
+{
+
+using ResolutionLayerKey = std::pair<uint8_t, uint16_t>;
+using QueueMap = std::map<ResolutionLayerKey, std::queue<mqcoder>>;
+
+struct BackupCache
+{
+  BackupCache(const mqcoder& coder, uint8_t passno, uint8_t position, uint16_t i, uint16_t k)
+      : coder_(coder), passno_(passno), position_(position), i_(i), k_(k)
+  {}
+  mqcoder coder_;
+  uint8_t passno_;
+  uint8_t position_;
+  uint16_t i_;
+  uint16_t k_;
+};
+
+struct LocationProbe
+{
+  uint8_t passno_ = 0xFF;
+  uint8_t position_ = 0xFF;
+  uint16_t i_ = 0xFFFF;
+  uint16_t k_ = 0xFFFF;
+
+  LocationProbe() = default;
+  LocationProbe(uint8_t passno, uint8_t position, uint16_t i, uint16_t k);
+
+  bool probe(uint8_t passno, uint8_t position, uint16_t i, uint16_t k) const;
+};
+
+class DebugContext
+{
+public:
+  static DebugContext& getInstance();
+
+  bool handle(const mqcoder& mq, uint8_t passno, uint8_t position, uint16_t i, uint16_t k);
+  void incrementDifferentialLayer();
+  void restoreBackup(void);
+
+  uint16_t compno_;
+  uint8_t resno_;
+  uint64_t precinctIndex_;
+  uint16_t layno_;
+  uint32_t cblkno_;
+  uint8_t numResolutions_;
+  uint16_t differentialLayers_;
+  bool debug_backup;
+  uint16_t maxLayers_;
+  LocationProbe probe_;
+
+private:
+  DebugContext();
+  void checkEmpty() const;
+  std::queue<mqcoder>& getQueue(uint16_t layer);
+  void logProbe(bool differential, uint16_t layer, size_t order, uint32_t c, uint8_t passno,
+                uint8_t position, uint16_t i, uint16_t k) const;
+
+  QueueMap referenceCoders_;
+  std::queue<BackupCache> backupQueue_;
+  size_t queueSize_[256] = {0}; // Initialize all elements to 0
+};
+
+} // namespace grk
