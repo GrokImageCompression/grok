d8c108f532f4b7911c930999574ba15e407c3e77
Grok Compression <support@grokcompression.com>
2025-11-27 10:14:10 -0500

    examples: add comparison method to core_simple

--- examples/core/core_simple.cpp
+++ examples/core/core_simple.cpp
@@ -26,6 +26,7 @@ with memory buffers as source and destination
 #include <cstring>
 #include <memory>
 #include <inttypes.h>
+#include <vector>
 
 #include "grok.h"
 
@@ -33,7 +34,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
 {
   const uint32_t dimX = 640;
   const uint32_t dimY = 480;
-  const uint32_t numComps = 3;
+  const auto colourSpace = GRK_CLRSPC_GRAY;
+  const uint32_t numComps = colourSpace == GRK_CLRSPC_SRGB ? 3 : 1;
   const uint32_t precision = 16;
 
   grk_object* codec = nullptr;
@@ -44,6 +46,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
 
   uint64_t compressedLength = 0;
 
+  bool images_equal = true;
+
   // 1. initialize library
   grk_initialize(nullptr, 0);
 
@@ -52,6 +56,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
   grk_compress_set_default_params(&compressParams);
   compressParams.cod_format = GRK_FMT_JP2;
   compressParams.verbose = true;
+  compressParams.irreversible = 0; // Enable reversible (lossless) compression
 
   grk_decompress_parameters decompressParams = {};
 
@@ -66,7 +71,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
   grk_stream_params inputStreamParams = {};
   inputStreamParams.buf = data.get();
 
-  // 4. create input image (blank)
+  // 4. create input image
   auto components = std::make_unique<grk_image_comp[]>(numComps);
   for(uint32_t i = 0; i < numComps; ++i)
   {
@@ -78,7 +83,10 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
     c->prec = precision;
     c->sgnd = false;
   }
-  inputImage = grk_image_new(numComps, components.get(), GRK_CLRSPC_SRGB, true);
+  inputImage = grk_image_new(numComps, components.get(), colourSpace, true);
+
+  // create original data buffers (contiguous, stride == width)
+  std::vector<std::unique_ptr<int32_t[]>> originalBuffers(numComps);
 
   // fill in component data: see grok.h header for full details of image structure
   for(uint16_t compno = 0; compno < inputImage->numcomps; ++compno)
@@ -92,18 +100,28 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
       fprintf(stderr, "Image has null data for component %d\n", compno);
       goto beach;
     }
-    // fill in component data, taking component stride into account
-    // in this example, we just zero out each component
-    auto srcData = new int32_t[(size_t)compWidth * compHeight];
-    memset(srcData, 0, (size_t)compWidth * compHeight * sizeof(int32_t));
-    auto srcPtr = srcData;
+
+    // allocate and fill original buffer with grid pattern (contiguous)
+    originalBuffers[compno] = std::make_unique<int32_t[]>((size_t)compWidth * compHeight);
+    auto srcPtr = originalBuffers[compno].get();
     for(uint32_t j = 0; j < compHeight; ++j)
     {
-      memcpy(destData, srcPtr, (size_t)compWidth * sizeof(int32_t));
+      for(uint32_t i = 0; i < compWidth; ++i)
+      {
+        srcPtr[i] = (i % 32 == 0 || j % 32 == 0) ? 65535 : 0;
+      }
       srcPtr += compWidth;
-      destData += comp->stride;
     }
-    delete[] srcData;
+
+    // copy from original buffer to input image data, respecting stride
+    auto destPtr = destData;
+    srcPtr = originalBuffers[compno].get();
+    for(uint32_t j = 0; j < compHeight; ++j)
+    {
+      memcpy(destPtr, srcPtr, (size_t)compWidth * sizeof(int32_t));
+      srcPtr += compWidth;
+      destPtr += comp->stride;
+    }
   }
 
   // 5. initialize compressor
@@ -149,6 +167,59 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
     goto beach;
   }
 
+  printf("Decompression succeeded\n");
+
+  // 10. compare original buffers and output image
+  if(inputImage->numcomps != outputImage->numcomps || inputImage->x0 != outputImage->x0 ||
+     inputImage->y0 != outputImage->y0 || inputImage->x1 != outputImage->x1 ||
+     inputImage->y1 != outputImage->y1 || inputImage->color_space != outputImage->color_space)
+  {
+    images_equal = false;
+  }
+  else
+  {
+    for(uint32_t compno = 0; compno < inputImage->numcomps; ++compno)
+    {
+      auto in_comp = inputImage->comps + compno;
+      auto out_comp = outputImage->comps + compno;
+      if(in_comp->dx != out_comp->dx || in_comp->dy != out_comp->dy || in_comp->w != out_comp->w ||
+         in_comp->h != out_comp->h || in_comp->prec != out_comp->prec ||
+         in_comp->sgnd != out_comp->sgnd)
+      {
+        images_equal = false;
+        break;
+      }
+      auto in_data = originalBuffers[compno].get();
+      auto in_stride = in_comp->w;
+      auto out_data = (int32_t*)out_comp->data;
+      auto out_stride = out_comp->stride;
+      bool comp_equal = true;
+      for(uint32_t j = 0; j < in_comp->h; ++j)
+      {
+        if(memcmp(in_data, out_data, in_comp->w * sizeof(int32_t)) != 0)
+        {
+          comp_equal = false;
+          break;
+        }
+        in_data += in_stride;
+        out_data += out_stride;
+      }
+      if(!comp_equal)
+      {
+        images_equal = false;
+        break;
+      }
+    }
+  }
+  if(images_equal)
+  {
+    printf("Input and output images are identical\n");
+  }
+  else
+  {
+    printf("Input and output images differ\n");
+  }
+
   rc = EXIT_SUCCESS;
 beach:
 
@@ -156,7 +227,7 @@ beach:
   grk_object_unref(codec);
   if(inputImage)
     grk_object_unref(&inputImage->obj);
-  // note: since inputImage was allocated by library, it will be cleaned
+  // note: since outputImage was allocated by library, it will be cleaned
   // up by library
   grk_deinitialize();
 
