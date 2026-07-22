845faed615d5c0f459e3525b75f94947f27dc39e
Grok Compression <support@grokcompression.com>
2026-07-21 20:37:22 -0400

    script update

--- integrations/gdal/debug.sh
+++ integrations/gdal/debug.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 GROK_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
 
-cd $HOME/src/grok-test-data
+cd  "$GROK_ROOT"/../grok-test-data
 git checkout master-debug
 cd "$GROK_ROOT"
 mkdir -p build && cd build
